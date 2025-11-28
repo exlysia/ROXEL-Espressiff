a9262353b8310d29d9db8fd2b935012bd5ee64a3
Yingxing_ <118210930+mafuyu-me@users.noreply.github.com>
2025-11-28 04:22:27 +0300

    Revert "28.11.2025 04:22"
    This reverts commit c25ef9a53d6c7656fefe65462444156a87dd105f.

--- components/ROXEL/include/requests/fast_machine.h
+++ components/ROXEL/include/requests/fast_machine.h
@@ -10,14 +10,13 @@
 class FastRequestMachine
 {
 public:
-    FastRequestMachine(uint16_t port, x10_cli_manager *cli_manager, QueueHandle_t *transaction_queue_handler);
+    FastRequestMachine(uint16_t port, x10_cli_manager *cli_manager);
     ~FastRequestMachine();
     bool load(RequestLoader *loader);
     void launch(void);
     void stop(void);
     bool isRunning(void) const;
 
-    QueueHandle_t *_transaction_queue_handler = NULL;
     TaskHandle_t _udp_server_task_handler = NULL;
 
     uint32_t *_request_hashes = nullptr;
@@ -25,7 +24,6 @@ public:
     size_t _request_count = 0;
 
     x10_cli_manager *_cli_manager;
-    uint32_t _identificator = 0;
     int _socket = -1;
 
     void _stop_server(void);
